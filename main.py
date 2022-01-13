ec36fd8e473212f2ab2031a37e01b687af693f37
zeroone2numeral2 <numeralzeroone@gmail.com>
2022-01-13 20:36:36 +0100

    fixed launching condition

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import bot
 
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     if sys.version_info[:3] < (3, 6, 2):
         print("Sorry, Python 3.6.2+ is required")
         sys.exit(0)
