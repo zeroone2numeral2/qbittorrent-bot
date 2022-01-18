c4ef6c564f2bcf001c7b429176ef85d9433e36de
zeroone2numeral2 <numeralzeroone@gmail.com>
2022-01-18 19:48:45 +0100

    actually use the new config validation thing

--- config.py
+++ config.py
@@ -18,18 +18,17 @@ def exit_with_message(message: str):
     sys.exit(0)
 
 
-if False:
-    example_config = toml.load('config.example.toml')
+example_config = toml.load('config.example.toml')
 
-    for config_section, example_config_section_dict in example_config.items():
-        if config_section not in config:
-            exit_with_message(f"missing section [{config_section}]")
+for config_section, example_config_section_dict in example_config.items():
+    if config_section not in config:
+        exit_with_message(f"missing section [{config_section}]")
 
-        config_section_dict = config[config_section]
-        for example_config_key, example_config_val in example_config_section_dict.items():
-            if example_config_key not in config_section_dict:
-                exit_with_message(f"missing key [{config_section}.{example_config_key}]")
+    config_section_dict = config[config_section]
+    for example_config_key, example_config_val in example_config_section_dict.items():
+        if example_config_key not in config_section_dict:
+            exit_with_message(f"missing key [{config_section}.{example_config_key}]")
 
-            config_val = config_section_dict[example_config_key]
-            if type(config_val) is not type(example_config_val):
-                exit_with_message(f"type of field [{config_section}.{example_config_key}] should be of type '{type(example_config_val).__name__}'")
+        config_val = config_section_dict[example_config_key]
+        if type(config_val) is not type(example_config_val):
+            exit_with_message(f"type of field [{config_section}.{example_config_key}] should be of type '{type(example_config_val).__name__}'")
