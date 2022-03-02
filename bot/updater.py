203d3770e8498f28971f17a7f19b739e3f50647f
Ariyan Eghbal <ariyan.eghbal@gmail.com>
2022-03-02 05:15:36 +0330

    Add proxy support
    Proxy support is added based on python-telegram-bot support for HTTP, HTTPS and Socks proxies
    Also [socks] dependency is added to requirements.txt as needed by python-telegram-bot to support socks
    leaving url empty disables proxy and leaving username empty disables authentication

--- bot/updater.py
+++ bot/updater.py
@@ -4,8 +4,20 @@ from telegram.ext import Defaults
 from .bot import CustomUpdater
 from config import config
 
+REQUEST_KWARGS = {}
+if config.proxy.url:
+    REQUEST_KWARGS['proxy_url'] = config.proxy.url 
+    if config.proxy.username:
+        REQUEST_KWARGS['urllib3_proxy_kwargs'] = {
+            'username': config.proxy.username,
+            'password': config.proxy.password 
+        }
+
+
+
 updater = CustomUpdater(
     token=config.telegram.token,
     defaults=Defaults(timeout=config.telegram.timeout, disable_web_page_preview=True),
-    workers=config.telegram.workers
+    workers=config.telegram.workers,
+    request_kwargs=REQUEST_KWARGS,
 )
